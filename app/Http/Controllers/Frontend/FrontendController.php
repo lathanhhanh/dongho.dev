4f92d333f0ab82e2afc1b017ef8d5fddd00a0751
Gấu Toroooo <lathanh4321@gmail.com>
2020-03-09 11:35:44 +0700

    show danh mục sản phẩm

--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class FrontendController extends Controller
 {
-    //
+    public function __construct()
+    {
+    }
 }
