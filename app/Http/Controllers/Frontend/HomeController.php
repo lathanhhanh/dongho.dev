89204f0d9fe2c165583a7c3ade066fd274b5ea2d
Gấu Toroooo <lathanh4321@gmail.com>
2020-03-09 16:08:23 +0700

    hiện thỉ sản phẩm hot ra trang chủ

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -10,14 +10,27 @@ class HomeController extends Controller
 {
     public function index()
     {
+
+        //sản phẩm mới
         $productsNew = Product::where('pro_active', 1)
             ->orderByDesc('id')
             ->limit(4)
             ->select('id','pro_name','pro_slug','pro_avatar','pro_price','pro_sale')
             ->get();
 
+        //sản phẩm hot
+        $productsHot = Product::where([
+            'pro_active' => 1,
+            'pro_hot' => 1
+        ])
+            ->orderByDesc('id')
+            ->limit(4)
+            ->select('id','pro_name','pro_slug','pro_avatar','pro_price','pro_sale')
+            ->get();
+
         $viewData = [
-            'productsNew' => $productsNew
+            'productsNew' => $productsNew,
+            'productsHot' => $productsHot
         ];
         return view('frontend.pages.home.index', $viewData);
     }
