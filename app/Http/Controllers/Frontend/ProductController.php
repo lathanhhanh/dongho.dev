bf875b61c8a99b32d4835547ec2c0c3f8d104be3
Gấu Toroooo <lathanh4321@gmail.com>
2020-03-11 22:31:07 +0700

    Hiển thị thuộc tính page product

--- app/Http/Controllers/Frontend/ProductController.php
+++ app/Http/Controllers/Frontend/ProductController.php
@@ -3,12 +3,30 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Attribute;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     public function index()
     {
-        return view('frontend.pages.product.index');
+        $attributes = $this->syncAttributeGroup();
+
+        $viewData = [
+           'attributes' => $attributes
+        ];
+        return view('frontend.pages.product.index', $viewData);
+    }
+
+    public function syncAttributeGroup()
+    {
+        $attributes = Attribute::get();
+        $groupAttribute = [];
+        foreach ($attributes as $key => $attribute)
+        {
+            $key = $attribute->gettype($attribute->atb_type)['name'];
+            $groupAttribute[$key][] = $attribute->toArray();
+        }
+        return $groupAttribute;
     }
 }
--- resources/views/frontend/pages/product/include/inc_sidebar.blade.php
+++ resources/views/frontend/pages/product/include/inc_sidebar.blade.php
@@ -3,298 +3,37 @@
 <div class="titleCol">Thương hiệu</div>
 <div class="filter">
     <ul class="ULGroup" id="CatGroup">
-
         <li>
             <label>
                 <input type="checkbox" value="594"  />
                 <h2><span>Đồng hồ Philippe Auguste</span></h2>
             </label>
         </li>
-
         <li>
             <label>
                 <input type="checkbox" value="563"  />
                 <h2><span>Đồng hồ Epos Swiss</span></h2>
             </label>
         </li>
-
-        <li>
-            <label>
-                <input type="checkbox" value="559"  />
-                <h2><span>Đồng hồ Atlantic Swiss</span></h2>
-            </label>
-        </li>
-
-        <li>
-            <label>
-                <input type="checkbox" value="557"  />
-                <h2><span>Đồng hồ Diamond D</span></h2>
-            </label>
-        </li>
-
-        <li>
-            <label>
-                <input type="checkbox" value="562"  />
-                <h2><span>Đồng hồ Aries Gold</span></h2>
-            </label>
-        </li>
-
-        <li>
-            <label>
-                <input type="checkbox" value="568"  />
-                <h2><span>Đồng hồ Jacques Lemans</span></h2>
-            </label>
-        </li>
-
-        <li>
-            <label>
-                <input type="checkbox" value="574"  />
-                <h2><span>Đồng hồ QQ</span></h2>
-            </label>
-        </li>
-
-        <li>
-            <label>
-                <input type="checkbox" value="573"  />
-                <h2><span>Jacques Du Manoir</span></h2>
-            </label>
-        </li>
-
-        <li>
-            <label>
-                <input type="checkbox" value="560"  />
-                <h2><span>Bruno Sohnle Glashutte</span></h2>
-            </label>
-        </li>
-
-        <li>
-            <label>
-                <input type="checkbox" value="62"  />
-                <h2><span>Đồng hồ Citizen</span></h2>
-            </label>
-        </li>
-
-        <li>
-            <label>
-                <input type="checkbox" value="558"  />
-                <h2><span>Stuhrling Tourbillon</span></h2>
-            </label>
-        </li>
-
-        <li>
-            <label>
-                <input type="checkbox" value="556"  />
-                <h2><span>Stuhrling Original Swiss</span></h2>
-            </label>
-        </li>
-
-        <li>
-            <label>
-                <input type="checkbox" value="553"  />
-                <h2><span>Tourbillon Memorigin</span></h2>
-            </label>
-        </li>
-
-        <li>
-            <label>
-                <input type="checkbox" value="549"  />
-                <h2><span>Đồng hồ Casio</span></h2>
-            </label>
-        </li>
-
     </ul>
 </div>
-<div class="titleCol">Dành cho</div>
-<div class="filter">
-    <ul class="ULGroup" id="LoaiGroup">
-
-        <li>
-            <label>
-                <input type="checkbox" value="1"  />
-                <span>Đồng hồ nam</span>
-            </label>
-        </li>
-
-        <li>
-            <label>
-                <input type="checkbox" value="2"  />
-                <span>Đồng hồ nữ</span>
-            </label>
-        </li>
-
-        <li>
-            <label>
-                <input type="checkbox" value="3"  />
-                <span>Đồng hồ đôi</span>
-            </label>
-        </li>
-
-        <li>
-            <label>
-                <input type="checkbox" value="5"  />
-                <span>Đồng hồ trẻ em</span>
-            </label>
-        </li>
-
-    </ul>
-</div>
-<div class="titleCol">Khoảng giá</div>
-<div class="filter">
-    <ul class="ULGroup" id="PriceGroup">
-        <li>
-            <label>
-                <input type="checkbox" value="16"  />
-                <span>Giá < 2 triệu</span>
-            </label>
-        </li>
-        <li>
-            <label>
-                <input type="checkbox" value="1"  />
-                <span>Giá từ 2 - 5 triệu</span>
-            </label>
-        </li>
-        <li>
-            <label>
-                <input type="checkbox" value="3"  />
-                <span>Giá từ 5 - 10 triệu</span>
-            </label>
-        </li>
-        <li>
-            <label>
-                <input type="checkbox" value="4"  />
-                <span>Giá từ 10 - 20 triệu</span>
-            </label>
-        </li>
-        <li>
-            <label>
-                <input type="checkbox" value="5"  />
-                <span>Giá > 20 triệu</span>
-            </label>
-        </li>
-
-    </ul>
-</div>
-<div class="titleCol">Năng lượng</div>
-<div class="filter">
-    <ul class="ULGroup" id="MacGroup">
-        <li>
-            <label>
-                <input type="checkbox" value="1"  />
-                <h2><span>Đồng hồ cơ</span></h2>
-            </label>
-        </li>
-        <li>
-            <label>
-                <input type="checkbox" value="2"  />
-                <h2><span>Đồng hồ điện tử</span></h2>
-            </label>
-        </li>
-
-    </ul>
-</div>
-<div class="titleCol">Loại dây</div>
-<div class="filter">
-    <ul class="ULGroup" id="WGroup">
-        <li>
-            <label>
-                <input type="checkbox" value="1"  />
-                <h4><span>Dây da</span></h4>
-            </label>
-        </li>
-        <li>
-            <label>
-                <input type="checkbox" value="2"  />
-                <h4><span>Thép không gỉ</span></h4>
-            </label>
-        </li>
-        <li>
-            <label>
-                <input type="checkbox" value="3"  />
-                <h4><span>Mạ vàng</span></h4>
-            </label>
-        </li>
-        <li>
-            <label>
-                <input type="checkbox" value="5"  />
-                <h4><span>Mạ đồng</span></h4>
-            </label>
-        </li>
-        <li>
-            <label>
-                <input type="checkbox" value="6"  />
-                <h4><span>Cao su</span></h4>
-            </label>
-        </li>
-        <li>
-            <label>
-                <input type="checkbox" value="7"  />
-                <h4><span>Dây nhựa</span></h4>
-            </label>
-        </li>
-        <li>
-            <label>
-                <input type="checkbox" value="8"  />
-                <h4><span>Dây vải, dù</span></h4>
-            </label>
-        </li>
-        <li>
-            <label>
-                <input type="checkbox" value="9"  />
-                <h4><span>Hợp kim thép</span></h4>
-            </label>
-        </li>
-
-    </ul>
-</div>
-<div class="titleCol">Loại vỏ</div>
-<div class="filter">
-    <ul class="ULGroup" id="VGroup">
-        <li>
-            <label>
-                <input type="checkbox" value="1"  />
-                <h4><span>Thép không gỉ</span></h4>
-            </label>
-        </li>
-        <li>
-            <label>
-                <input type="checkbox" value="2"  />
-                <h4><span>Mạ vàng</span></h4>
-            </label>
-        </li>
-        <li>
-            <label>
-                <input type="checkbox" value="3"  />
-                <h4><span>Vàng nguyên khối</span></h4>
-            </label>
-        </li>
-        <li>
-            <label>
-                <input type="checkbox" value="5"  />
-                <h4><span>Mạ đồng</span></h4>
-            </label>
-        </li>
-        <li>
-            <label>
-                <input type="checkbox" value="6"  />
-                <h4><span>Cao su</span></h4>
-            </label>
-        </li>
-        <li>
-            <label>
-                <input type="checkbox" value="7"  />
-                <h4><span>Vỏ nhựa</span></h4>
-            </label>
-        </li>
-        <li>
-            <label>
-                <input type="checkbox" value="10"  />
-                <h4><span>Hợp kim thép</span></h4>
-            </label>
-        </li>
-
-    </ul>
-</div>
-
+    @if(isset($attributes))
+        @foreach($attributes as $key => $attribute )
+            <div class="titleCol">{{ $key }}</div>
+            <div class="filter">
+                <ul class="ULGroup" id="LoaiGroup">
+                    @foreach ($attribute as $item)
+                        <li>
+                            <label>
+                                <input type="checkbox" value="{{ $item['id'] }}"  />
+                                <span>{{ $item['atb_name'] }}</span>
+                            </label>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+        @endforeach
+    @endif
 <script>
     $( ".colleft .ULGroup input" ).on("click",function() {
         var UrlActive = $("#strUrl99").val();
